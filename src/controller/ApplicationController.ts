01fc9485f86ac078f46f4e096691dc79e74d53bf
Phil J. Laszkowicz <phil@fillip.pro>
2017-09-06 16:32:07 +0300

    Added ApplicationDelegate.

--- src/controller/ApplicationController.ts
+++ src/controller/ApplicationController.ts
@@ -1,5 +1,24 @@
-abstract class ApplicationController {
+abstract class ApplicationController implements ApplicationDelegate {
+    
+    private readonly runnable: () => void;
 
+    constructor(runnable: () => void) {
+        this.runnable = runnable;
+    }
+
+    applicationWillLaunch() {
+        throw new Error("Method not implemented.");
+    }
+    
+    applicationDidLaunch() {
+        throw new Error("Method not implemented.");
+    }
+
+    private run(runnable: () => void) {
+        if (this.runnable) {
+            runnable();
+        }
+    }
 }
 
 export default ApplicationController;
---
+++ src/controller/ApplicationDelegate.ts
@@ -0,0 +1,4 @@
+interface ApplicationDelegate {
+    applicationWillLaunch();
+    applicationDidLaunch();
+}

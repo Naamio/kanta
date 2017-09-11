1dec782284dd7bb7b5b4d63906351ee20b5b4822
Phil J. Laszkowicz <phil@fillip.pro>
2017-09-11 16:28:37 +0300

    Removed function from ApplicationController constructor params.

--- src/controller/ApplicationController.ts
+++ src/controller/ApplicationController.ts
@@ -2,12 +2,18 @@ import ApplicationDelegate from "./ApplicationDelegate";
 
 abstract class ApplicationController implements ApplicationDelegate {
     
+<<<<<<< HEAD
     static shared: ApplicationController;
 
     private _runnable: () => void;
 
     constructor() {
         ApplicationController.shared = this;
+=======
+    private runnable: () => void;
+
+    constructor() {
+>>>>>>> 0ca6f44... Removed function from ApplicationController constructor params.
     }
 
     applicationWillLaunch() {
@@ -19,9 +25,15 @@ abstract class ApplicationController implements ApplicationDelegate {
     }
 
     run(runnable: () => void) {
+<<<<<<< HEAD
         this._runnable = runnable;
 
         if (this._runnable) {
+=======
+        this.runnable = runnable;
+
+        if (this.runnable) {
+>>>>>>> 0ca6f44... Removed function from ApplicationController constructor params.
             runnable();
         }
     }

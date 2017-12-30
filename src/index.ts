68880232ca02cfc95de990ea59a531258ea8e7d8
Phil J. Laszkowicz <phil@fillip.pro>
2017-12-30 19:05:05 +0200

    Added cookies to exported objects.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { ApplicationController, ApplicationDelegate, Controller, ViewController } from "./controller";
 export { Observation, ObservationCenter, ObservationName } from "./observations"
 export { Repository } from "./repository"
+export { Cookies } from "./state"
 export { View } from "./view";
